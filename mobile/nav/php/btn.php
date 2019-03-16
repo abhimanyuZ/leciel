7af1f4f83685fd6d4d62db1e3394632b8d143411
abhimanyu <abhimanyukrsingh00@gmail.com>
2019-03-16 23:41:20 +0530

    Create btn.php

---
+++ mobile/nav/php/btn.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Button</title>
+</head>
+<body>
+
+<form  action="./e_reg.php" method="POST">
+	
+	<input type="text" name="promo"><br>
+
+	<input type="submit" name="event" value="1"><br><br>
+	<input type="submit" name="event" value="2"><br><br>
+	<input type="submit" name="event" value="3"><br><br>
+	<input type="submit" name="event" value="4"><br><br>
+	<input type="submit" name="event" value="5"><br><br>
+	<input type="submit" name="event" value="6"><br><br>
+
+</form>
+
+</body>
+</html>
